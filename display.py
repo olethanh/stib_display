915fedea53e56cce63d3bde09253f26a3b4a6952
Olivier Le Thanh Duong <olivier@lethanh.be>
2013-04-28 18:00:49 +0200

    Initial commit

---
+++ display.py
@@ -0,0 +1,47 @@
+from time import sleep
+from itertools import chain
+
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.cleanup()
+
+status = {}
+ins = [8, 10, 12]
+outs = [7,5,3]
+leds_list = list(chain(*[[(i,o) for o in outs] for i in ins]))
+
+def set(channel, state):
+    if channel not in status:
+        GPIO.setup(channel, GPIO.OUT, initial=state)
+    else:
+        GPIO.output(channel, state)
+        print 'set ', channel, 'to ', state
+        status[channel] = state
+
+def on(channel):
+    set(channel, GPIO.HIGH)
+
+def off(channel):
+    set(channel, GPIO.LOW)
+
+def reset():
+    for i in ins: off(i)
+    for o in outs: on(o)
+
+def led(i):
+    on(leds_list[i][0])
+    off(leds_list[i][1])
+
+def ledoff(i):
+    off(leds_list[i][0])
+    on(leds_list[i][1])
+
+def cycle(leds, rows, cols):
+    for r in range(rows):
+        for u in range(r*cols, (r+1)*cols):
+            if u in leds:
+                led(u)
+        sleep(10.0/1000)
+        reset()
+
---
+++ stib.py
@@ -0,0 +1,39 @@
+import urllib2
+import BeautifulSoup
+from collections import OrderedDict as odict
+
+
+station_led = [
+    u'trone',
+    u'science',
+    u'luxembourg',
+    u'idalie',
+    u'blyckaerts',
+    u'germoir',
+    u'rodin',
+    u'delporte',
+    u'etterbeek gare',
+]
+
+def get_stations():
+    url_heiligenborre = "http://m.stib.be/linelight.php?line=95&iti=1&lang=fr"
+    page = urllib2.urlopen(url_heiligenborre).read()
+    soup = BeautifulSoup.BeautifulSoup(page)
+    return odict([(li.text.lower(), dict(li.attrs).get('class') == 'active') for li in soup('li')])
+
+
+def leds_from_stations(r):
+    return [station_led.index(k) for k,v in r.iteritems() if v and k in station_led]
+
+stations = get_stations()
+
+print [k for k,v in stations.iteritems() if v]
+print station_led
+
+leds = leds_from_stations(stations)
+print leds
+
+#import r as display
+#while True: display.cycle(leds, 3,3)
+
+
